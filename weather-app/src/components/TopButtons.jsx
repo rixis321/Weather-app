35f0f5a7bcd0867ed7f9f1b41b81a9ed6e9d4e52
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-28 01:43:59 +0100

    Added basics

---
+++ weather-app/src/components/TopButtons.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function TopButtons() {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "London",
+    },
+    {
+      id: 3,
+      title: "Sydney",
+    },
+    {
+      id: 4,
+      title: "Tokyo",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-around">
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
